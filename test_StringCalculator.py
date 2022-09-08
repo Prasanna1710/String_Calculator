f86ea414af1e986e2743d483a02020ed1529fd5f
Prasanna1710 <90819864+Prasanna1710@users.noreply.github.com>
2022-09-08 06:10:13 +0530

    Creat test_StringCalculator.py
    This file will be used to test the code written in String_Calculator

---
+++ test_StringCalculator.py
@@ -0,0 +1,16 @@
+# This file will be used to test the code written in String_Calculator.py 
+# This is the file at initial commit
+
+import unittest
+import String_Calculator
+
+
+class TestString_Calculator(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+      
+      
+if __name__ == '__main__':
+    unittest.main()
