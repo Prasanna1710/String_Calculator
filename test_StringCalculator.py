a32f69c0e4fc8fe0afe8616aeee2b1a366880672
Prasanna1710 <90819864+Prasanna1710@users.noreply.github.com>
2022-09-08 07:54:47 +0530

    Update test_String_Calculator for Testcase 6
    here we are adding tests to ignore numbers greater than 1000

--- test_StringCalculator.py
+++ test_StringCalculator.py
@@ -9,8 +9,11 @@ class TestString_Calculator(unittest.TestCase):
 
     def setUp(self):
         pass
+    def test_add_str(self):
+        self.assertEqual(String_Calculator.add(['1', '2', '4', '8','1','1000']), 1016)
+        self.assertEqual(String_Calculator.add(['6', '21', '3', '11','10000']), 41)
+        self.assertEqual(String_Calculator.add(['1', '26', '4', '8', '1','1009','3827','21718']), 40)
 
-      
       
 if __name__ == '__main__':
     unittest.main()
