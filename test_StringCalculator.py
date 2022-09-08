6051180aaded3922e3e11df62cbf4a9035cf40fc
Prasanna1710 <90819864+Prasanna1710@users.noreply.github.com>
2022-09-08 07:33:52 +0530

    Update test_StringCalculator for Testcase 4
    here we have added  tests to show that upon entering a number which is negative function will throw exception

--- test_StringCalculator.py
+++ test_StringCalculator.py
@@ -9,6 +9,12 @@ class TestString_Calculator(unittest.TestCase):
 
     def setUp(self):
         pass
+    def test_add_N(self):
+        with self.assertRaises(Exception):
+            String_Calculator.add(['1','2','-1'])
+        self.assertEqual(String_Calculator.add(['1','2']),3)
+        with self.assertRaises(Exception):
+            String_Calulator.add(['1','2','-1','-2','-3'])
 
       
       
