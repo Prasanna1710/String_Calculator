42fad53e2a9918456a76e41d3b124eebd682d044
Prasanna1710 <90819864+Prasanna1710@users.noreply.github.com>
2022-09-08 08:31:46 +0530

    Update String_Calculator of main
    merged all testcases from 1 to 6 in main

--- String_Calculator .py
+++ String_Calculator .py
@@ -10,7 +10,26 @@ def addString(num_str):
     return 0
   elif num_str.isdigit():
     return int(num_str)
-  
+def add(num_str):
+  sum = 0
+  flag = 0
+  negative_nums = ''
+  for i in range(len(num_str)):
+    if num_str[i].isdigit():
+      if int(num_str[i]) > 1000:
+        continue
+      else:
+        sum += int(num_str[i])
+    elif num_str[i].islower():
+      sum += (ord(num_str[i]) - 96)
+    elif num_str[i].isupper():
+      raise Exception('Only lowercase alphabets allowed')
+    elif int(num_str[i]) < 0:
+      flag = 1
+      negative_nums += num_str[i] + ' '
+  if flag == 1:
+    raise Exception('Negative numbers not allowed, negative numbers are' + negative_nums)
+  return sum
 
   
   
