099d217adc39c95b9c48709fe15322c3b98e3a87
Prasanna1710 <90819864+Prasanna1710@users.noreply.github.com>
2022-09-08 07:58:35 +0530

    Update String_Calculator for Testcase 6
    added a code to ignore numbers > 1000

--- String_Calculator .py
+++ String_Calculator .py
@@ -1,3 +1,14 @@
 # at Initial commit of code
 
-def add(num_str)
+def add(num_str):
+  def add(num_str):
+  # code for test case 3
+  Sum = 0
+  for i in range(len(arr)):
+    if arr[i].isdigit():
+      if int(arr[i]) > 1000:
+        continue
+      else:
+        Sum += int(arr[i])
+  return Sum
+    
