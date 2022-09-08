98b3f46830df9c61a0db599eab34d63e416fb0dc
Prasanna1710 <90819864+Prasanna1710@users.noreply.github.com>
2022-09-08 06:22:50 +0530

    Update String_Calculator for testcase 1
    created addString function which returns int for 1 input and 0 for blank input

--- String_Calculator .py
+++ String_Calculator .py
@@ -1,3 +1,20 @@
 # at Initial commit of code
 
-def add(num_str)
+# Taking comma Seperated inputs
+num = input("enter comma seperated inputs:")
+num = list(map(str,num.split(',')))
+
+
+def addString(num_str):
+  if num_str == '':
+    return 0
+  elif num_str.isdigit():
+    return int(num_str)
+  
+
+  
+  
+  
+if __name__ == '__main__':
+  pass
+  
