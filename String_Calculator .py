377b7b7b276c4b2b7a6082a698b4693904c58ecb
Prasanna1710 <90819864+Prasanna1710@users.noreply.github.com>
2022-09-08 06:53:06 +0530

    Update String_Calculator for test case3

--- String_Calculator .py
+++ String_Calculator .py
@@ -1,3 +1,12 @@
 # at Initial commit of code
 
-def add(num_str)
+def add(num_str):
+  # code for test case 3
+  sum = 0
+  for i in range(len(arr)):
+    if arr[i].isdigit():
+      sum += int(arr[i])
+    elif arr[i].islower():
+      sum += (ord(arr[i])-96)
+    elif arr[i].isupper():
+      raise Exception('Only lowercase alphabets allowed')
