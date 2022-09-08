0637ec6f7c1d85868428984b07b89e111d26a181
Prasanna1710 <90819864+Prasanna1710@users.noreply.github.com>
2022-09-08 08:55:56 +0530

    Update test_StringCalculator.py

--- test_StringCalculator.py
+++ test_StringCalculator.py
@@ -19,17 +19,17 @@ class TestString_Calculator(unittest.TestCase):
     def test_add(self):
         # test case 2 and part 3 of test case 1
         # test case where we add 2 numbers together
-        self.assertEqual(StringCal.add(['1', '2']), 3)
-        self.assertEqual(StringCal.add(['1', '2', '4', '8']), 15)
-        self.assertEqual(StringCal.add(['1', '2', '-3','6']), 6)
-        self.assertEqual(StringCal.add(['-1', '-2']), -3)
+        self.assertEqual(String_Calculator.add(['1', '2']), 3)
+        self.assertEqual(String_Calculator.add(['1', '2', '4', '8']), 15)
+        self.assertEqual(String_Calculator.add(['1', '2', '-3','6']), 6)
+        self.assertEqual(String_Calculator.add(['-1', '-2']), -3)
 
     # test case 3
     def test_add_str(self):
-        self.assertEqual(StringCal.add(['1', '2', '4', '8', 'a']), 16)
-        self.assertEqual(StringCal.add(['f', 'u', 'c', 'k']), 41)
-        self.assertEqual(StringCal.add(['1', 'z', '4', '8', 'a']), 40)
-        self.assertEqual(StringCal.add(['a', 'b', 'c', '8', 'a']), 15)
+        self.assertEqual(String_Calculator.add(['1', '2', '4', '8', 'a']), 16)
+        self.assertEqual(String_Calculator.add(['f', 'u', 'c', 'k']), 41)
+        self.assertEqual(String_Calculator.add(['1', 'z', '4', '8', 'a']), 40)
+        self.assertEqual(String_Calculator.add(['a', 'b', 'c', '8', 'a']), 15)
         with self.assertRaises(Exception):
             StringCal.add(['b', 'c', 'd', 'F', 'a'])
 
@@ -44,9 +44,15 @@ class TestString_Calculator(unittest.TestCase):
 
     # test case 6
     def test_add_1000(self):
-        self.assertEqual(StringCal.add(['1', '2', '4', '8','1000']), 1015)
-        self.assertEqual(StringCal.add(['1', '2', '4', '8','2234']), 15)
-        self.assertEqual(StringCal.add(['1', '2', '4', '8','1233','1220202','999']), 1014)
+        self.assertEqual(String_Calculator.add(['1', '2', '4', '8','1000']), 1015)
+        self.assertEqual(String_Calculator.add(['1', '2', '4', '8','2234']), 15)
+        self.assertEqual(String_Calculator.add(['1', '2', '4', '8','1233','1220202','999']), 1014)
+    
+    # tets case 7
+    # test case for n numbers - newline separated
+    def test_addstr_newline(self):
+        self.assertEqual(String_Calculator.addString('1','\n2'), 3)
+
       
       
 if __name__ == '__main__':
