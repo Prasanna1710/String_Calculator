f2152ad59b28c5b2f8d9e874b28ac2d34aeb454d
Prasanna1710 <90819864+Prasanna1710@users.noreply.github.com>
2022-09-08 06:49:34 +0530

    Update tests for test case 3
    here test_StringCalculator file is updated with tests for test case 3

--- test_StringCalculator.py
+++ test_StringCalculator.py
@@ -9,6 +9,14 @@ class TestString_Calculator(unittest.TestCase):
 
     def setUp(self):
         pass
+    # tests for test case 3
+    def test_add_str(self):
+        self.assertEqual(String_Calculator.add(['1', '2', '4', '8','a']), 16)
+        self.assertEqual(String_Calculator.add(['f', 'u', 'c', 'k']), 41)
+        self.assertEqual(String_Calculator.add(['1', 'z', '4', '8', 'a']), 40)
+        self.assertEqual(String_Calculator.add(['a', 'b', 'c', '8', 'a']), 15)
+        with self.assertRaises(Exception):
+            StringCalculator.add(['b', 'c', 'd', 'F', 'a'])
 
       
       
