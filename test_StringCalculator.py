469c824c5396c1eed1f47fefac53ad0281dd23b0
Prasanna1710 <90819864+Prasanna1710@users.noreply.github.com>
2022-09-08 06:35:48 +0530

    Update test_StringCalculator for test case 2
    Here test case 2 and part of test case 1 are covered where the str array containing 2 or more numbers is at the input

--- test_StringCalculator.py
+++ test_StringCalculator.py
@@ -9,6 +9,13 @@ class TestString_Calculator(unittest.TestCase):
 
     def setUp(self):
         pass
+    # test case 2
+    def test_add(self):
+        # test case where we add 2 numbers together
+        self.assertEqual(String_Calculator.add(['1', '2']), 3)
+        self.assertEqual(String_Calculator.add(['1', '2', '4', '8']), 15)
+        self.assertEqual(String_Calculator.add(['1', '2', '-3','6']), 6)
+        self.assertEqual(String_Calculator.add(['-1', '-2']), -3)
 
       
       
