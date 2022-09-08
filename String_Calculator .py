dc37581c73ddb6e27405b4e3acb9b3d5a64a2b6d
Prasanna1710 <90819864+Prasanna1710@users.noreply.github.com>
2022-09-08 07:39:16 +0530

    Update String_Calculator for Testcase 4
    updated code of String_Calculator for test case 4 where exception is thrown when negative numbers are detected

--- String_Calculator .py
+++ String_Calculator .py
@@ -1,3 +1,19 @@
 # at Initial commit of code
 
-def add(num_str)
+def add(num_str):
+  Sum = 0
+  flag = 0
+  negative_nums = ''
+  for i in range(len(num_str)):
+    if num_str[i].isdigit():
+      Sum += int(num_str[i])
+    elif num_str[i].islower():
+      Sum += (ord(num_str[i]) - 96)
+    elif num_str[i].isupper():
+      raise Exception('Only lowercase alphabets allowed')
+    elif int(num_str[i]) < 0:
+      flag = 1
+      negative_nums += num_str[i] + ' '
+  if flag == 1:
+      raise Exception('Negative numbers not allowed, negative numbers are' + negative_nums)
+  return Sum
