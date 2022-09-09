1e1072e6c026420021890f64ea103b2045222bc5
Prasanna1710 <90819864+Prasanna1710@users.noreply.github.com>
2022-09-09 15:33:25 +0530

    Update StringToArray.py

--- StringToArray.py
+++ StringToArray.py
@@ -1,18 +1,21 @@
 a = "//[***]\n1***22***33"
 
 def String_to_array(str):
+    i = 0
     arr = []
-    for i in range(len(str)):
-        a = str[i]
+    while i < len(str):
         if str[i] == '\n':
             continue
         if str[i].isdigit():
             if i+1 <= len(str)-1 and str[i+1].isdigit():
                 arr.append(str[i:i+2])
-                i = i+1
+                i = i + 2
             else:
                 arr.append(str[i])
+                i += 1
+        i += 1
     print(arr)
+
 
 
 String_to_array(a)
