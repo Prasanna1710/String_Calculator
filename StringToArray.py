dbbb286d2c8a38154082cf6a8824bcedfb47af87
Prasanna1710 <90819864+Prasanna1710@users.noreply.github.com>
2022-09-09 15:14:34 +0530

    StringtoArray commit

---
+++ StringToArray.py
@@ -0,0 +1,20 @@
+a = "//[***]\n1***22***33"
+
+def String_to_array(str):
+    arr = []
+    for i in range(len(str)):
+        a = str[i]
+        if str[i] == '\n':
+            continue
+        if str[i].isdigit():
+            if i+1 <= len(str)-1 and str[i+1].isdigit:
+                arr.append(str[i:i+2])
+                i = i+1
+            else:
+                arr.append(str[i])
+    print(arr)
+
+
+String_to_array(a)
+
+print('*'.isdigit())
